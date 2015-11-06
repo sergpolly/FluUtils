8d16e5245e3c9a9ee8335cf2bb9a2a4610213b98
sergpolly <sergpolly@gmail.com>
2015-11-06 10:26:26 -0500

    bug fixed, handles complex splice sites

--- generate_SNP_cache.py
+++ generate_SNP_cache.py
@@ -71,31 +71,49 @@ def isin_orf(orf,pos):
         # assuming total length of the ORF is ~3
         assert sum((stop-start+1) for start,stop in orf_bounds)%3 == 0
         # if that holds, start analysis ...
-        for start,stop in orf_bounds:
-            # NOT CORRECT TO BE EDITED ................!!!!!!!!!!!!!!!!!!!
-            if start <= pos <= stop:
-                # which codon ...
-                codon = (pos-start)//3
-                # codon coordinates are ...
-                codon_start = codon*3 + start
-                # c1 = codon*3 + start + 1
-                codon_stop  = codon*3 + start + 2
-                # which nucleotide position "pos" corresponds to?
-                codon_shift = pos - codon_start
-                # store stuff in  ...
-                which_orf.append( (orf_id, (codon,codon_shift,slice(codon_start,codon_stop+1)) ) )
+        orf_positions = sum([range(start,stop+1) for start,stop in orf_bounds], [])
+        # pos residence in ORF or not in ORF?
+        if pos in orf_positions:
+            # relative pos, simply an index in the list of orf_positions ...
+            relative_pos = orf_positions.index(pos)
+            # the index of the codon it belongs to ...
+            codon_idx = relative_pos//3
+            # relative codon coordinates (indexes within CDS):
+            relative_codon_coord = [codon_idx*3+i for i in range(3)]
+            # pos is 1st,2nd or 3rd position in the codon?
+            codon_shift = relative_pos - relative_codon_coord[0]
+            # absolute codon coordinates (indexes in the genome):
+            codon_coord = [orf_positions[_] for _ in relative_codon_coord]
+            which_orf.append( (orf_id, (codon_idx,codon_shift,codon_coord) ) )
     return dict(which_orf)
 
 
 
+# def isin_orf(orf,pos):
+#     """checks if a given position is in any of the ORFs.
+#     Returns list of the ORFs that it participates in with the codon coords."""
+#     which_orf = []
+#     for orf_id in orf:
+#         orf_bounds = orf[orf_id]
+#         # assuming total length of the ORF is ~3
+#         assert sum((stop-start+1) for start,stop in orf_bounds)%3 == 0
+#         # if that holds, start analysis ...
+#         for start,stop in orf_bounds:
+#             # NOT CORRECT TO BE EDITED ................!!!!!!!!!!!!!!!!!!!
+#             if start <= pos <= stop:
+#                 # which codon ...
+#                 codon = (pos-start)//3
+#                 # codon coordinates are ...
+#                 codon_start = codon*3 + start
+#                 # c1 = codon*3 + start + 1
+#                 codon_stop  = codon*3 + start + 2
+#                 # which nucleotide position "pos" corresponds to?
+#                 codon_shift = pos - codon_start
+#                 # store stuff in  ...
+#                 which_orf.append( (orf_id, (codon,codon_shift,slice(codon_start,codon_stop+1)) ) )
+#     return dict(which_orf)
 
 
-# before fixing the UTR problem seg lengthes were :
-# seg_lengths = {'seg1_H1N1':2314, 'seg2_H1N1':2302, 'seg3_H1N1':2203, 'seg4_H1N1':1776, 'seg5_H1N1':1497, 'seg6_H1N1':1427, 'seg7_H1N1':1006, 'seg8_H1N1':870}
-
-
-
-#NONVARIABLE THINGS GOES FIRST ...
 
 # genome_fname = "../FluGenomes/H1N1FULL.fa"
 genome_fname = "../FluGenomes/H1N1FULL.fa"
@@ -111,7 +129,7 @@ stop_codons = dict([ (codon,'STOP') for codon in CodonTable.standard_dna_table.s
 genetic_code.update(stop_codons)
 
 # mutate genome position
-for seg in h1n1.segments:#[0:1]:
+for seg in h1n1.segments:
     seg_len = h1n1.seglen[seg]
     segment = h1n1.genome[seg]
     the_orf = h1n1.orf[seg]
@@ -125,12 +143,14 @@ for seg in h1n1.segments:#[0:1]:
                 line_out = "%s_%s%d%s NORF"%(seg,ref_nt,pos+1,mut_nt)
                 print line_out
         else:
-            #
+            # might be inefficient, but works for our output ...
             for mut_nt in mut_nucs:
                 line_out = "%s_%s%d%s"%(seg,ref_nt,pos+1,mut_nt)
                 for product in inorf:
-                    aa_idx, codon_shift, codon_slice = inorf[product]
-                    the_codon = segment[codon_slice]
+                    aa_idx, codon_shift, codon_coord = inorf[product]
+                    # we use explicit coordinates for all 3 nucs in codon,
+                    # to handle within codon splice sites properly ...
+                    the_codon = ''.join(segment[_] for _ in codon_coord)
                     ref_aa = genetic_code[the_codon]
                     # modifying the ref codon to mutant ...
                     mut_codon = list(the_codon)
@@ -141,14 +161,6 @@ for seg in h1n1.segments:#[0:1]:
                     line_out += " %s_%s%d%s"%(product,ref_aa,aa_idx+1,mut_aa)
                 print line_out
 
-
-
-# gc = CodonTable.unambiguous_dna_by_id
-# from Bio.Data import CodonTable
-
-# is it in either of the ORFs ?
-
-# if so, determine amino acid change there ...
 
 
 
